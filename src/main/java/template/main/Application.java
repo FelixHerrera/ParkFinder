f89d2049910983161d1c5ee78d0af00cbacebfe9
Jonathan McElroy <jonathanpmcelroy@gmail.com>
2015-11-20 19:37:44 -0600

    Customizes the location of the cache db

--- src/main/java/template/main/Application.java
+++ src/main/java/template/main/Application.java
@@ -5,6 +5,8 @@ import org.springframework.core.io.ClassPathResource;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.config.DiskStoreConfiguration;
 import net.sf.ehcache.config.PersistenceConfiguration;
 import net.sf.ehcache.config.PersistenceConfiguration.Strategy;
 import net.sf.ehcache.store.MemoryStoreEvictionPolicy;
@@ -18,14 +20,15 @@ public class Application {
 
 	public void run() {
 		// TODO JONATHAN: I have not figured out how to use the spring api with this, so we are creating it manually
-		CacheManager cm = CacheManager.create();
+		String path = "src/main/resources/db";
+		CacheManager cm = CacheManager.create(new Configuration().diskStore(new DiskStoreConfiguration().path(path)));
 		
 		Cache getPlaceDetailsCache = new Cache(
 				new CacheConfiguration("getPlaceDetails", 1000) // Create a new cache that can contain 1000 objects 
 				    .memoryStoreEvictionPolicy(MemoryStoreEvictionPolicy.LFU) 
 				    .eternal(false)  
 				    .diskExpiryThreadIntervalSeconds(3600)
-				    .diskPersistent(true)); 
+				    .diskPersistent(true));
 		cm.addCache(getPlaceDetailsCache);
 		
 		@SuppressWarnings("resource")
